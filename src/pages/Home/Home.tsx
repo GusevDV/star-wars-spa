a53cdd3a66b962755026e5f46e930094cef4d59b
Dmitry Gusev <dvgusev@hotmail.com>
2023-04-17 22:10:08 +0300

    Added card to the home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,38 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Grid, LinkBox, LinkOverlay } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import { swapi } from 'shared/api';
 
 const Home = () => {
   const { data } = swapi.useGetAllPeopleQuery();
 
-  console.log(data);
-
-  return <Box>Welcome</Box>;
+  return (
+    <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+      {data?.results.map((person) => (
+        <LinkBox
+          as={Box}
+          key={person.name}
+          boxShadow="md"
+          h={40}
+          bg="gray.50"
+          borderRadius={10}
+          textAlign="center"
+          color="gray.900"
+          p={2}
+          transitionDuration="fast"
+          transitionTimingFunction="ease-out"
+          _hover={{
+            textDecoration: 'none',
+            transform: 'translateY(-0.3rem)',
+            boxShadow: 'xl',
+          }}
+        >
+          <LinkOverlay as={Link} to={'/'}>
+            {person.name}
+          </LinkOverlay>
+        </LinkBox>
+      ))}
+    </Grid>
+  );
 };
 
 export default Home;
