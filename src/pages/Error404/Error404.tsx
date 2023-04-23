88fbd2ef804225ac8535ee18fd4b3a454bca945f
Dmitry Gusev <dvgusev@hotmail.com>
2023-04-23 12:00:18 +0300

    Updated Error404 page

--- src/pages/Error404/Error404.tsx
+++ src/pages/Error404/Error404.tsx
@@ -3,8 +3,8 @@ import { Heading, Text } from '@chakra-ui/react';
 const Error404 = () => {
   return (
     <>
-      <Heading>Страницы не существует</Heading>
-      <Text>Проверьте адрес сайта или найдите нужный раздел на главной странице</Text>
+      <Heading>Page not found</Heading>
+      <Text>Check the site address</Text>
     </>
   );
 };
