165ff353778841b4b4aa81d5acd079976cd3869b
Dmitry Gusev <dvgusev@hotmail.com>
2023-04-23 17:51:57 +0300

    Updated layout on personal page

--- src/pages/Person/Person.tsx
+++ src/pages/Person/Person.tsx
@@ -1,4 +1,15 @@
-import { Box, Divider, Flex, Grid, Heading, Skeleton, Text } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Card,
+  Divider,
+  Flex,
+  Grid,
+  Heading,
+  SimpleGrid,
+  Skeleton,
+  Text,
+} from '@chakra-ui/react';
 import moment from 'moment';
 import { useParams } from 'react-router-dom';
 import { People, swapi } from 'shared/api';
@@ -21,33 +32,54 @@ export const Person = () => {
 
   return (
     <>
-      <Skeleton isLoaded={!isLoading}>
-        <Flex justifyContent="space-between" alignItems="center" py={[2, 4]} flexWrap="wrap">
-          <Heading pr={6} color="gray.800">
-            {data.name}
-          </Heading>
-          <Box>
-            <Text color="gray.400" fontSize="sm">
-              created: {moment(data.created).format('MMM Do YYYY, h:mm:ss a')}
-            </Text>
-            <Text color="gray.400" fontSize="sm">
-              edited: {moment(data.edited).format('MMM Do YYYY, h:mm:ss a')}
-            </Text>
-          </Box>
-        </Flex>
-      </Skeleton>
-      <Grid
-        mt={10}
-        templateColumns={{ md: 'repeat(2, 1fr)', lg: 'repeat(4, 1fr)', xl: 'repeat(5, 1fr)' }}
-        gap={6}
-      >
-        {filteredKeys.map((key) => {
-          const name = snakeCaseToPhrase(key);
-          const value = data[key];
-          return (
-            <PeopleProperty key={key} name={name} value={typeof value === 'string' ? value : ''} />
-          );
-        })}
+      <Grid gridTemplateColumns={['1fr', '1fr', '2fr 4fr']} gap={10}>
+        <Card p={10} bg="gray.50">
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            textAlign="center"
+            py={[2, 4]}
+            gap={4}
+            flexWrap="wrap"
+          >
+            <Avatar size={['xl', '2xl']} />
+            <Heading as="h1" size="lg">
+              {data.name}
+            </Heading>
+            <Box>
+              <Text color="gray.500" fontSize="sm">
+                created: {moment(data.created).format('MMM Do YYYY, h:mm:ss a')}
+              </Text>
+              <Text color="gray.500" fontSize="sm">
+                edited: {moment(data.edited).format('MMM Do YYYY, h:mm:ss a')}
+              </Text>
+            </Box>
+          </Flex>
+        </Card>
+        <Box>
+          <Text as="h2" fontSize="lg">
+            Personal information
+          </Text>
+          <Divider mt={2} />
+          <Grid
+            mt={4}
+            templateColumns={{ md: 'repeat(2, 1fr)', lg: 'repeat(4, 1fr)', xl: 'repeat(5, 1fr)' }}
+            gap={6}
+          >
+            {filteredKeys.map((key) => {
+              const name = snakeCaseToPhrase(key);
+              const value = data[key];
+              return (
+                <PeopleProperty
+                  key={key}
+                  name={name}
+                  value={typeof value === 'string' ? value : ''}
+                />
+              );
+            })}
+          </Grid>
+        </Box>
       </Grid>
     </>
   );
